ee60f97483eded0431fbf0d76c02d31053578495
Dominik Geldmacher <dominik.geldmacher@saint-gobain.com>
2022-03-15 16:09:59 +0100

    navigation progress

--- frontend/src/components/generics/Navigation.vue
+++ frontend/src/components/generics/Navigation.vue
@@ -3,7 +3,7 @@
     <div class="px-2 mx-auto max-w-7xl sm:px-6 lg:px-8">
       <div class="relative flex items-center justify-between h-16">
         <!-- Mobile menu button-->
-        <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
+        <div class="absolute inset-y-0 left-0 flex items-center md:hidden">
           <DisclosureButton
             class="inline-flex items-center justify-center p-2 text-gray-400 rounded-md hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
           >
@@ -13,27 +13,29 @@
           </DisclosureButton>
         </div>
 
-        <!-- Main menu + LOGO -->
-        <div class="flex items-center justify-center flex-1 sm:items-stretch sm:justify-start">
+        <!-- 'MainMenu + Darkmode + Logo' Block -->
+        <div class="flex flex-1 gap-x-10 sm:items-stretch sm:justify-start md:justify-center md:items-center">
+          <!-- LOGO -->
           <div class="flex items-center flex-shrink-0">
             <img
-              class="block object-contain w-auto h-16 animate-pulse animate slow lg:hidden"
+              class="block object-contain w-auto h-16 ml-9 animate-pulse animate slow md:hidden"
               src="../../assets/cyberise_logo_centered_small_resized_for_web.png"
               alt="logo"
             />
             <img
-              class="hidden object-contain w-auto h-16 animate-pulse animate slow lg:block"
+              class="hidden object-contain w-auto h-16 bg-red-300 animate-pulse animate slow md:block"
               src="../../assets/cyberise_logo_centered_small_resized_for_web.png"
               alt="logo"
             />
           </div>
 
-          <div class="desktop:block desktop:ml-6 mobile:hidden">
-            <div class="flex space-x-4">
+          <!-- Main menu -->
+          <div class="hidden md:block md:ml-6">
+            <div class="flex items-center space-x-4">
               <router-link
                 v-for="item in navigation"
                 :key="item.name"
-                class
+                class=""
                 :to="item.to"
                 :class="[
                   item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
@@ -45,9 +47,9 @@
               </router-link>
             </div>
           </div>
-        </div>
 
-        <DarkModeSwitch />
+          <DarkModeSwitch />
+        </div>
 
         <div class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
           <!-- <button type="button" class="p-1 text-gray-400 bg-gray-800 rounded-full hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
@@ -81,20 +83,21 @@
       </div>
     </div>
 
-    <DisclosurePanel class="desktop:hidden laptop:hidden">
-      <div class="px-2 pt-2 pb-3 space-y-1">
-        <DisclosureButton
-          v-for="item in navigation"
-          :key="item.name"
-          as="router-link"
-          :to="item.to"
-          :class="[
-            item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-            'block px-3 py-2 rounded-md text-base font-medium'
-          ]"
-          :aria-current="item.current ? 'page' : undefined"
-        >
-          {{ item.name }}
+    <DisclosurePanel class="z-10 md:hidden">
+      <div class="flex flex-col items-start justify-center px-2 pt-2 pb-3 space-y-1 gap-y-1">
+        <DisclosureButton v-for="item in navigation" :key="item.name">
+          <router-link
+            :key="item.name"
+            class=""
+            :to="item.to"
+            :class="[
+              item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+              'px-3 py-2 rounded-md text-sm font-medium'
+            ]"
+            :aria-current="item.current ? 'page' : undefined"
+          >
+            {{ item.name }}
+          </router-link>
         </DisclosureButton>
       </div>
     </DisclosurePanel>
